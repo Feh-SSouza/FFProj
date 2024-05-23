a22f83bb9a06bf16a22c413886999b716115387c
Felipe Sposito <felipesposito@hotmail.com.br>
2024-05-23 14:35:57 -0300

    backup 23-05-24 flavor

--- app/routes.py
+++ app/routes.py
@@ -13,13 +13,39 @@ import secrets
 
 def init_app(app):
 
+    @app.route('/', methods=["GET", "POST"])
+    def index():
+        form = LoginForm()
 
+        if form.validate_on_submit():
+            user = usuario.query.filter_by(email=form.email.data).first()
 
-    @app.route('/')
+            if not user:
+                flash("Email do usuário está incorreto")
+                return redirect(url_for("index"))
+            
+            elif not check_password_hash(user.senha, form.senha.data):
+                flash("Senha do usuário está incorreta")
+                return redirect(url_for("index"))
+            
+            login_user(user, remember=form.remember.data, duration=timedelta(days=7))
+
+            return redirect(url_for("inicio"))
+        return render_template("index.html", form=form)
+    
+    @app.route("/logout")
+    def logout():
+        logout_user()
+        return redirect(url_for("index"))
+    
+
+    @app.route('/inicio')
+    @login_required
     def inicio():
         return render_template('inicio.html')
     
     @app.route('/sobre')
+    @login_required
     def sobre():
         return render_template('sobre.html')
     
@@ -31,6 +57,7 @@ def init_app(app):
         return redirect(url_for('lista_receita'))
     
     @app.route('/receitas')
+    @login_required
     def lista_receita():
         return render_template('receitas.html', receitas=db.session.execute(db.select(receitas).order_by(receitas.id)).scalars())
     
@@ -71,3 +98,43 @@ def init_app(app):
             db.session.commit()
             return redirect(url_for("lista_receita"))
         return render_template('atualizar_receita.html', atualiza=atualiza)
+    
+    @app.route('/caduser', methods=["POST", "GET"])
+    def cad_usuario():
+        if request.method == "POST":
+            user = usuario()
+            user.email = request.form["email"]
+            user.nome = request.form["nome"]        
+            user.senha = generate_password_hash(request.form["senha"])
+            db.session.add(user)
+            db.session.commit()
+                            
+            flash("Usuario criado com sucesso!")       
+            return redirect(url_for("cad_usuario"))
+        return render_template("cad_user.html")
+    
+    @app.route('/usuarios')
+    def lista_usuarios():
+        return render_template('usuarios.html', usuario=db.session.execute(db.select(usuario).order_by(usuario.id)).scalars())
+    
+    @app.route('/excluiruser/<int:id>')
+    def excluir_usuario(id):
+        delete=usuario.query.filter_by(id=id).first()
+        db.session.delete(delete)
+        db.session.commit()
+        return redirect(url_for('lista_usuarios'))
+    
+    @app.route('/atualizaruser/<int:id>', methods=["GET", "POST"])
+    def atualizar_usuario(id):
+        atualiza=usuario.query.filter_by(id=id).first()
+        if request.method == "POST":
+            nome_user = request.form["nome"]
+            email_user = request.form["email"]
+            senha_user = generate_password_hash(request.form["senha"])
+
+            flash("Usuário alterado com sucesso!")
+
+            atualiza.query.filter_by(id=id).update({"nome":nome_user, "email":email_user, "senha":senha_user})
+            db.session.commit()
+            return redirect(url_for("lista_usuarios"))
+        return render_template('atualizar_usuario.html', atualiza=atualiza)
